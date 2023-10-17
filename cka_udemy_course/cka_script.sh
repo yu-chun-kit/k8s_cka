64a0a4ed6e505b7c56cce77231f9c2e1aa71fea0
yu-chun-kit <johnson37siumen@hotmail.com>
2023-10-17 10:10:18 +0800

    add (part of) kubernetes components install and option view method

---
+++ cka_udemy_course/cka_script.sh
@@ -0,0 +1,26 @@
+wget https://storage.googleapis.com/kubernetes-release/release/v1.13.0/linux/amd64/kube-controller-manager
+
+kubectl get pods -n kube-system
+# view kube-controller-manager-options - kubeadm
+cat /etc/kubernetes/minifests/kube-controller-manager.yaml
+# node-monitor-period=5s
+# node-monitor-grace-period=40s
+# pod-eviction-timeout=5m0s
+
+# view controller-manager options
+cat /etc/systemd/system/kube-controller-manager.service
+# or
+ps -aux | grep kube-controller-manager
+
+wget https://storage.googleapis.com/kubernetes-release/release/v1.13.0/bin/linux/amd64/kube-scheduler
+
+# view kube-scheduler options - kubeadm
+cat /etc/kubernetes/manifests/kube-scheduler.yaml
+# or 
+ps -aux | grep kube-scheduler
+
+# installing kubelet
+wget https://storage.googleapis.com/kubernetes-release/v1.13.0/bin/linux/amd64/kubelet
+
+# view kubelet options
+ps -aux | grep kubelet
